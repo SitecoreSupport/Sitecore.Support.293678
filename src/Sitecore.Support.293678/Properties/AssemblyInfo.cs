3609ae9e8da4da54c984c47079ade49508b7e209
DK\mao <mao@sitecore.net>
2018-11-21 15:57:38 +0200

    Initial commit

---
+++ src/Sitecore.Support.293678/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.293678")]
+[assembly: AssemblyProduct("Sitecore.Support.293678")]
+[assembly: ComVisible(false)]
